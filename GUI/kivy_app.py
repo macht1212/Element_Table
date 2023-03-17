b4732898875634775bff5d7b74d3172b1a6af23c
Alexander Nazimov <nasimov.alexander@gmail.com>
2023-03-17 14:29:58 +0300

    changed kivy file

--- GUI/kivy_app.py
+++ GUI/kivy_app.py
@@ -11,52 +11,16 @@ from kivy.uix.label import Label
 
 from SQL.select_info import select_info
 
+from info.info import font_color, non_metal, alkaline, alkaline_earth, noble, semimetals, halogens, transition
+from info.info import posttransition, lanthanides, actinides
+
+from info.info import elements, non_metal_elements, alkaline_elements, alkaline_earth_elements, noble_elements
+from info.info import semimetals_elements, halogens_elements, transition_elements, posttransition_elements
+from info.info import lanthanides_elements, actinides_elements, num2, num1
+
 Config.set('graphics', 'resizable', 0)
 Config.set('graphics', 'width', 1300)
 Config.set('graphics', 'height', 840)
-
-
-font_color = [0, 0, 0, 1]
-
-non_metal = [143 / 255, 250 / 255, 139 / 255, 1]
-alkaline = [252 / 255, 77 / 255, 83 / 255, 1]
-alkaline_earth = [255 / 255, 215 / 255, 157 / 255, 1]
-noble = [181 / 255, 255 / 255, 255 / 255, 1]
-semimetals = [193 / 255, 194 / 255, 136 / 255, 1]
-halogens = [255 / 255, 255 / 255, 136 / 255, 1]
-transition = [253 / 255, 176 / 255, 178 / 255, 1]
-posttransition = [193 / 255, 193 / 255, 193 / 255, 1]
-lanthanides = [253 / 255, 172 / 255, 255 / 255, 1]
-actinides = [253 / 255, 130 / 255, 193 / 255, 1]
-other = [143 / 255, 250 / 255, 139 / 255, 1]
-
-elements = ['H', None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 'He',
-            'Li', 'Be', None, None, None, None, None, None, None, None, None, None, 'B', 'C', 'N', 'O', 'F', 'Ne',
-            'Na', 'Mg', None, None, None, None, None, None, None, None, None, None, 'Al', 'Si', 'P', 'S', 'Cl', 'Ar',
-            'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
-            'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe',
-            'Cs', 'Ba', None, 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn',
-            'Fr', 'Ra', None, 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og',
-            None, '57-71', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu',
-            None,
-            None, '89-103', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr',
-            None]
-
-non_metal_elements = ['H', 'C', 'N', 'O', 'P', 'S', 'Se']
-alkaline_elements = ['Li', 'Na', 'K', 'Rb', 'Cs', 'Fr']
-alkaline_earth_elements = ['Be', 'Mg', 'Ca', 'Sr', 'Ba', 'Ra']
-noble_elements = ['He', 'Ne', 'Ar', 'Kr', 'Xe', 'Rn', 'Og']
-semimetals_elements = ['B', 'Si', 'Ge', 'As', 'Sb', 'Te', 'Po']
-halogens_elements = ['F', 'Cl', 'Br', 'I', 'At', 'Ts']
-transition_elements = ['La', 'Ac', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
-                       'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd',
-                       'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg',
-                       'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn']
-posttransition_elements = ['Al', 'Ga', 'In', 'Sn', 'Tl', 'Pb', 'Bi', 'Nh', 'Fl', 'Mc', 'Lv']
-lanthanides_elements = ['Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu']
-actinides_elements = ['Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr']
-num1 = ['57-71']
-num2 = ['89-103']
 
 
 class PeriodicTableApp(App):
@@ -121,9 +85,9 @@ class PeriodicTableApp(App):
             curr = db.cursor()
 
             self.symbol = select_info(curr, Symbol=instance.text)
-            self.update_lable()
+            self.update_label()
 
-    def update_lable(self):
+    def update_label(self):
         self.lbl.text = f"""
 Atomic Number: {self.symbol[0]}
 Element: {self.symbol[1]},
---
+++ info/info.py
@@ -0,0 +1,41 @@
+font_color = [0, 0, 0, 1]
+
+non_metal = [143 / 255, 250 / 255, 139 / 255, 1]
+alkaline = [252 / 255, 77 / 255, 83 / 255, 1]
+alkaline_earth = [255 / 255, 215 / 255, 157 / 255, 1]
+noble = [181 / 255, 255 / 255, 255 / 255, 1]
+semimetals = [193 / 255, 194 / 255, 136 / 255, 1]
+halogens = [255 / 255, 255 / 255, 136 / 255, 1]
+transition = [253 / 255, 176 / 255, 178 / 255, 1]
+posttransition = [193 / 255, 193 / 255, 193 / 255, 1]
+lanthanides = [253 / 255, 172 / 255, 255 / 255, 1]
+actinides = [253 / 255, 130 / 255, 193 / 255, 1]
+other = [143 / 255, 250 / 255, 139 / 255, 1]
+
+elements = ['H', None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 'He',
+            'Li', 'Be', None, None, None, None, None, None, None, None, None, None, 'B', 'C', 'N', 'O', 'F', 'Ne',
+            'Na', 'Mg', None, None, None, None, None, None, None, None, None, None, 'Al', 'Si', 'P', 'S', 'Cl', 'Ar',
+            'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
+            'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe',
+            'Cs', 'Ba', None, 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn',
+            'Fr', 'Ra', None, 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og',
+            None, '57-71', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu',
+            None,
+            None, '89-103', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr',
+            None]
+
+non_metal_elements = ['H', 'C', 'N', 'O', 'P', 'S', 'Se']
+alkaline_elements = ['Li', 'Na', 'K', 'Rb', 'Cs', 'Fr']
+alkaline_earth_elements = ['Be', 'Mg', 'Ca', 'Sr', 'Ba', 'Ra']
+noble_elements = ['He', 'Ne', 'Ar', 'Kr', 'Xe', 'Rn', 'Og']
+semimetals_elements = ['B', 'Si', 'Ge', 'As', 'Sb', 'Te', 'Po']
+halogens_elements = ['F', 'Cl', 'Br', 'I', 'At', 'Ts']
+transition_elements = ['La', 'Ac', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
+                       'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd',
+                       'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg',
+                       'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn']
+posttransition_elements = ['Al', 'Ga', 'In', 'Sn', 'Tl', 'Pb', 'Bi', 'Nh', 'Fl', 'Mc', 'Lv']
+lanthanides_elements = ['Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu']
+actinides_elements = ['Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr']
+num1 = ['57-71']
+num2 = ['89-103']
