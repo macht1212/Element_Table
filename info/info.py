1be2b41ddd375ae467cc72729ab59d0fc5d83c3c
Alexander Nazimov <nasimov.alexander@gmail.com>
2023-04-22 22:12:03 +0300

    minor updates

--- info/info.py
+++ info/info.py
@@ -1,16 +1,19 @@
 font_color = [0, 0, 0, 1]
 
-non_metal = [143 / 255, 250 / 255, 139 / 255, 1]
-alkaline = [252 / 255, 77 / 255, 83 / 255, 1]
-alkaline_earth = [255 / 255, 215 / 255, 157 / 255, 1]
-noble = [181 / 255, 255 / 255, 255 / 255, 1]
-semimetals = [193 / 255, 194 / 255, 136 / 255, 1]
-halogens = [255 / 255, 255 / 255, 136 / 255, 1]
-transition = [253 / 255, 176 / 255, 178 / 255, 1]
-posttransition = [193 / 255, 193 / 255, 193 / 255, 1]
-lanthanides = [253 / 255, 172 / 255, 255 / 255, 1]
-actinides = [253 / 255, 130 / 255, 193 / 255, 1]
-other = [143 / 255, 250 / 255, 139 / 255, 1]
+# Button's colors
+colors = {
+    'non_metal': [143 / 255, 250 / 255, 139 / 255, 1],
+    'alkaline': [252 / 255, 77 / 255, 83 / 255, 1],
+    'alkaline_earth': [255 / 255, 215 / 255, 157 / 255, 1],
+    'noble': [181 / 255, 255 / 255, 255 / 255, 1],
+    'semimetals': [193 / 255, 194 / 255, 136 / 255, 1],
+    'halogens': [255 / 255, 255 / 255, 136 / 255, 1],
+    'transition': [253 / 255, 176 / 255, 178 / 255, 1],
+    'posttransition': [193 / 255, 193 / 255, 193 / 255, 1],
+    'lanthanides': [253 / 255, 172 / 255, 255 / 255, 1],
+    'actinides': [253 / 255, 130 / 255, 193 / 255, 1],
+    'other': [143 / 255, 250 / 255, 139 / 255, 1]
+}
 
 elements = ['H', None, None, None, None, None, None, None,
             None, None, None, None, None, None, None, None, None, 'He',
@@ -28,17 +31,22 @@ elements = ['H', None, None, None, None, None, None, None,
             'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og',
             None, '57-71', 'La', 'Ce', 'Pr', 'Nd', 'Pm',
             'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu',
-            None,
-            None, '89-103', 'Ac', 'Th', 'Pa', 'U',
-            'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr',
+            None, None, '89-103', 'Ac', 'Th', 'Pa', 'U','Np',
+            'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr',
             None]
 
 non_metal_elements = ['H', 'C', 'N', 'O', 'P', 'S', 'Se']
+
 alkaline_elements = ['Li', 'Na', 'K', 'Rb', 'Cs', 'Fr']
+
 alkaline_earth_elements = ['Be', 'Mg', 'Ca', 'Sr', 'Ba', 'Ra']
+
 noble_elements = ['He', 'Ne', 'Ar', 'Kr', 'Xe', 'Rn', 'Og']
+
 semimetals_elements = ['B', 'Si', 'Ge', 'As', 'Sb', 'Te', 'Po']
+
 halogens_elements = ['F', 'Cl', 'Br', 'I', 'At', 'Ts']
+
 transition_elements = ['La', 'Ac', 'Sc', 'Ti', 'V',
                        'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
                        'Y', 'Zr', 'Nb', 'Mo', 'Tc',
@@ -47,15 +55,20 @@ transition_elements = ['La', 'Ac', 'Sc', 'Ti', 'V',
                        'Ir', 'Pt', 'Au', 'Hg',
                        'Rf', 'Db', 'Sg', 'Bh', 'Hs',
                        'Mt', 'Ds', 'Rg', 'Cn']
+
 posttransition_elements = ['Al', 'Ga', 'In', 'Sn', 'Tl',
                            'Pb', 'Bi', 'Nh', 'Fl', 'Mc', 'Lv']
+
 lanthanides_elements = ['Ce', 'Pr', 'Nd', 'Pm', 'Sm',
                         'Eu', 'Gd', 'Tb', 'Dy', 'Ho',
                         'Er', 'Tm', 'Yb', 'Lu']
+
 actinides_elements = ['Th', 'Pa', 'U', 'Np', 'Pu',
                       'Am', 'Cm', 'Bk', 'Cf', 'Es',
                       'Fm', 'Md', 'No', 'Lr']
+
 num1 = ['57-71']
+
 num2 = ['89-103']
 
 text = ['Element: ', '', 'Atomic Mass: ', '',
--- info/screen_info.py
+++ info/screen_info.py
@@ -10,13 +10,13 @@ def screen_info():
     for m in get_monitors():
         spl = str(m).split('(')
         spl1 = spl[1].split(')')
-        dict_ = {}
+        screen_information = {}
         spl2 = spl1[0].split(',')
         for el in spl2:
             res = el.split('=')
-            dict_[res[0]] = res[1]
+            screen_information[res[0]] = res[1]
 
-    return dict_
+    return screen_information
 
 
 def screen():
